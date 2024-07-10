c030e3a16e06a7583605cd2b728bacc78e192d27
Hari Babu M <103945712+Hari574@users.noreply.github.com>
2024-07-10 12:41:18 +0530

    test

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-import seaborn as sbn
+import seaborn as sns
+import numpy as np
+from matplotlib import pyplot as plt
+
+rs = np.random.RandomState(10)
+d = rs.normal(size=10)
+
+def draw_hist_with_kde():
+    sns.histplot(d, kde=True, color='r')
+
+
+def draw_hist_with_rug():
+    # sns.distplot(d, rug=True, hist=True, kde=True)
+    sns.displot(d, rug=True, color='orange', kind='hist')
+
+if __name__=="__main__":
+    # draw_hist_with_kde()
+    # draw_hist_with_rug()
+    
+    plt.show()
+    read = input('Enter to exit')
